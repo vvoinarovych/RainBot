63fe838245b235e1a448d5a5abdc1d19579d062c
Walerij <vvoinarovych@gmail.com>
2022-03-23 20:35:45 +0100

    added bot to project

---
+++ src/main/java/com/github/telegram_rain_bot/TelegramRainBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.telegram_rain_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramRainBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramRainBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/telegram_rain_bot/TelegramRainBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.telegram_rain_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramRainBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
